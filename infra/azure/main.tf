8148e815ed3294149ea2ae5c71a4d4e3683c4565
Justin Doctolero <jdoctolero1@gmail.com>
2026-04-03 08:55:11 -0400

    refactor: rename terraform modules for better clarity.

--- infra/azure/main.tf
+++ infra/azure/main.tf
@@ -1,14 +1,14 @@
-module "kubernetes_rg" {
-  source   = "./modules/rg"
+module "kube_rg" {
+  source   = "./modules/resource_group"
   name     = "rg-${var.environment}-kubernetes-${var.location}"
   location = var.location
   tags     = local.merged_tags
 }
 
-module "kcp_vm" {
-  source              = "./modules/vm"
+module "kube_cp_vm" {
+  source              = "./modules/virtual_machine"
   vm_name             = "vm-${var.environment}-kcp-${var.location}-01"
-  resource_group_name = module.kubernetes_rg.name
+  resource_group_name = module.kube_rg.name
   location            = var.location
   subnet_id           = data.azurerm_subnet.subnet_admin.id
   private_ip          = var.kcp_vm_ip
@@ -23,11 +23,11 @@ module "kcp_vm" {
   tags = var.tags
 }
 
-module "kwn_vm" {
-  source              = "./modules/vm"
+module "kube_worker_vm" {
+  source              = "./modules/virtual_machine"
   count               = length(var.kwn_vm_ips)
   vm_name             = "vm-${var.environment}-kwn-${var.location}-0${count.index + 1}"
-  resource_group_name = module.kubernetes_rg.name
+  resource_group_name = module.kube_rg.name
   location            = var.location
   subnet_id           = data.azurerm_subnet.subnet_app.id
   private_ip          = var.kwn_vm_ips[count.index]
@@ -41,21 +41,21 @@ module "kwn_vm" {
   tags                = var.tags
 }
 
-module "pip" {
-  source              = "./modules/pip"
+module "kube_pip" {
+  source              = "./modules/public_ip"
   public_ip_name      = "pip-${var.environment}-kube-${var.location}-01"
-  resource_group_name = module.kubernetes_rg.name
+  resource_group_name = module.kube_rg.name
   location            = var.location
   tags                = var.tags
 }
 
-module "agw" {
-  source              = "./modules/agw"
+module "kube_agw" {
+  source              = "./modules/app_gateway"
   agw_name            = "agw-${var.environment}-kube-${var.location}-01"
-  resource_group_name = module.kubernetes_rg.name
+  resource_group_name = module.kube_rg.name
   location            = var.location
   subnet_id           = data.azurerm_subnet.subnet_app_gateway.id
-  public_ip_id        = module.pip.id
+  public_ip_id        = module.kube_pip.id
   tags                = var.tags
   sku_name            = "Basic"
   sku_tier            = "Basic"
