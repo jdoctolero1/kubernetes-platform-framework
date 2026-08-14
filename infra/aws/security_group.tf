538ce55ed107d5367ef950483168a28e58f94ce2
Justin Doctolero <jdoctolero1@gmail.com>
2026-08-13 20:26:37 -0400

    task: adding kubernetes ports needed for control plane and worker nodes

--- infra/aws/security_group.tf
+++ infra/aws/security_group.tf
@@ -19,6 +19,69 @@ module "security_group_control_plane" {
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block]
+    },
+    {
+      description = "Allow Kubernetes API Server"
+      from_port   = 6443
+      to_port     = 6443
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow etcd"
+      from_port   = 2379
+      to_port     = 2380
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block]
+    },
+    {
+      description = "Allow Kubelet API"
+      from_port   = 10250
+      to_port     = 10250
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow kube-scheduler"
+      from_port   = 10251
+      to_port     = 10251
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block]
+    },
+    {
+      description = "Allow kube-controller-manager"
+      from_port   = 10252
+      to_port     = 10252
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block]
+    },
+    {
+      description = "Allow Calico BGP"
+      from_port   = 179
+      to_port     = 179
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Calico Typha"
+      from_port   = 5473
+      to_port     = 5473
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Cilium VXLAN"
+      from_port   = 8472
+      to_port     = 8472
+      protocol    = "udp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Cilium Metrics"
+      from_port   = 4240
+      to_port     = 4240
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
     }
   ]
 
@@ -48,6 +111,55 @@ module "security_group_worker_nodes" {
       to_port     = 22
       protocol    = "tcp"
       cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block]
+    },
+    {
+      description = "Allow Kubelet API"
+      from_port   = 10250
+      to_port     = 10250
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Read-only Kubelet API"
+      from_port   = 10255
+      to_port     = 10255
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow NodePort Services"
+      from_port   = 30000
+      to_port     = 32767
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Calico BGP"
+      from_port   = 179
+      to_port     = 179
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Calico Typha"
+      from_port   = 5473
+      to_port     = 5473
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Cilium VXLAN"
+      from_port   = 8472
+      to_port     = 8472
+      protocol    = "udp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
+    },
+    {
+      description = "Allow Cilium Metrics"
+      from_port   = 4240
+      to_port     = 4240
+      protocol    = "tcp"
+      cidr_blocks = [data.aws_subnet.admin_subnet.cidr_block, data.aws_subnet.app_subnet.cidr_block]
     }
   ]
 
